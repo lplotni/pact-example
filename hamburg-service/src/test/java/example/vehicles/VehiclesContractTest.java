636c4db4d37e08747cffecf91a47c27624553e00
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2017-07-09 22:09:06 +0200

    Using the pact-broker also in spring-provider testing

--- hamburg-service/src/test/java/example/vehicles/VehiclesContractTest.java
+++ hamburg-service/src/test/java/example/vehicles/VehiclesContractTest.java
@@ -3,6 +3,7 @@ package example.vehicles;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.RestPactRunner;
 import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 import au.com.dius.pact.provider.spring.target.MockMvcTarget;
@@ -15,7 +16,8 @@ import static org.mockito.Mockito.when;
 
 @RunWith(RestPactRunner.class)
 @Provider("hamburg-service")
-@PactFolder("../pacts")
+//@PactFolder("../pacts")
+@PactBroker(host="localhost", port="80")
 public class VehiclesContractTest {
 
     private RegistrationRepository registrations;
@@ -32,7 +34,7 @@ public class VehiclesContractTest {
        mockMvcTarget.setControllers(vehiclesCtrl);
     }
 
-    @State("data for HH from 2017 exists")
+    @State("data for HH from 2017 exists") //what will happen if change that?
     public void dataFrom2017() {
         when(registrations.findAll()).thenReturn(VehicleRegistrations.builder()
                 .city("Hamburg")
